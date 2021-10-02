39b97327be739f9e69e6f0b013d37b8d30124cbe
M-JM <45374451+M-JM@users.noreply.github.com>
2021-10-02 07:52:49 +0200

    First NUnit Test
    Installed the required Nuget packages and performed first Nunit Test

---
+++ FirstNUnitTest/CalculatorNUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstUnitTest
+{
+
+    [TestFixture]
+   public class CalculatorNUnitTest
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_OutputGetCorrectAddition()
+        {
+            //Arrange - intialisation
+            Calculator calc = new();
+
+            //Act
+
+            int result = calc.AddNumbers(10, 20);
+
+            //Assert
+
+            Assert.AreEqual(30, result);
+        }
+
+    }
+}
